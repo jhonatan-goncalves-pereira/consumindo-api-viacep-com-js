decbda974ae9f1e16fa82112cfd8570eeb391da6
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-05-31 19:24:17 -0300

    Formatando requisição em uma função assícrona, visto boa prática para não entrar no callback hell

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/");
 // Retorno em Promise
 // promessa de algo a acontecer, tornando algo assíncrono em algo síncrono 
 // o uso do .then() duplo retorna promessas que então resultam em um retorno tangível
-consultaCep
-.then(resposta =>{ 
-    return resposta.json();
-}).then(dados => {
-    console.log(dados)
-});
+// consultaCep
+// .then(resposta =>resposta.json())
+// .then(dados => console.log(dados)).finally();
 
+async function consultarCep(){
+    var reqCep = await consultaCep;
+    reqCepConvertida = reqCep.json();
+    console.log(reqCepConvertida);
+}
 
+consultarCep();
 
 
 /*
